e35993aa81d3bf860ce998cf9c115545cc255210
Ciprian Khlud <ciprian.mustiata@yahoo.com>
2018-01-30 21:12:20 +0200

    Moved code.

--- src/Installer/Installer/MainWindow.xaml.cs
+++ src/Installer/Installer/MainWindow.xaml.cs
@@ -1,7 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.IO;
-using System.IO.Compression;
 using System.Linq;
 using System.Reflection;
 using System.Text;
@@ -40,46 +39,12 @@ namespace Installer
         {
             using (var zipStream = GetStream("Installer.h2demo.zip"))
             {
-                UnzipFromStream(zipStream, "out");
+                ZipExtractorUtilies.UnzipFromStream(zipStream, "out");
             }
         }
         /// <summary>
         /// Copies the contents of input to output. Doesn't close either stream.
         /// </summary>
-        public static void CopyStream(Stream input, Stream output)
-        {
-            byte[] buffer = new byte[8 * 1024];
-            int len;
-            while ((len = input.Read(buffer, 0, buffer.Length)) > 0)
-            {
-                output.Write(buffer, 0, len);
-            }
-        }
-        public void UnzipFromStream(Stream zipStream, string outFolder)
-        {
-            Directory.CreateDirectory(outFolder);
-            var za = new ZipArchive(zipStream, ZipArchiveMode.Read);
-            foreach (var zipArchiveEntry in za.Entries)
-            {
-                var words = zipArchiveEntry.Name.Split('/');
-                if(words.Length<2)
-                    continue;
-                Directory.CreateDirectory(words[0]);
-            }
-
-            foreach (var zipArchiveEntry in za.Entries)
-            {
-                
-                using (var childStream = zipArchiveEntry.Open())
-                {
-                    using (Stream file = File.Create(zipArchiveEntry.Name))
-                    {
-                        CopyStream(childStream, file);
-                    }
-                }
-            }
-        }
-
         private void OnCloseClick(object sender, RoutedEventArgs e)
         {
             throw new NotImplementedException();
---
+++ src/Installer/Installer/ZipExtractorUtilies.cs
@@ -0,0 +1,43 @@
+﻿using System.IO;
+using System.IO.Compression;
+
+namespace Installer
+{
+    class ZipExtractorUtilies
+    {
+        public static void CopyStream(Stream input, Stream output)
+        {
+            byte[] buffer = new byte[8 * 1024];
+            int len;
+            while ((len = input.Read(buffer, 0, buffer.Length)) > 0)
+            {
+                output.Write(buffer, 0, len);
+            }
+        }
+        public static void UnzipFromStream(Stream zipStream, string outFolder)
+        {
+            Directory.CreateDirectory(outFolder);
+            var za = new ZipArchive(zipStream, ZipArchiveMode.Read);
+            foreach (var zipArchiveEntry in za.Entries)
+            {
+                var words = zipArchiveEntry.Name.Split('/');
+                if (words.Length < 2)
+                    continue;
+                Directory.CreateDirectory(Path.Combine(outFolder, words[0]));
+            }
+
+            foreach (var zipArchiveEntry in za.Entries)
+            {
+
+                using (var childStream = zipArchiveEntry.Open())
+                {
+                    using (Stream file = File.Create(Path.Combine(outFolder, zipArchiveEntry.Name)))
+                    {
+                        CopyStream(childStream, file);
+                    }
+                }
+            }
+        }
+
+    }
+}
