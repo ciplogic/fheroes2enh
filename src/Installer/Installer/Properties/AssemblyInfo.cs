81fc66002e9709094c998622d2d074341e58bab5
ciplogic <ciprian.mustiata@gmail.com>
2018-02-14 02:48:46 +0200

    Fix on crash related with serialization

--- src/Installer/Installer/Properties/AssemblyInfo.cs
+++ src/Installer/Installer/Properties/AssemblyInfo.cs
@@ -7,7 +7,7 @@ using System.Windows;
 // General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("Installer")]
+[assembly: AssemblyTitle("FHeroes2Enh Installer")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
