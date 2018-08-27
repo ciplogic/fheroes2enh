6f7473d8dfe48a926bcde36607f28ab1eca7c5cc
ciplogic <ciprian.mustiata@gmail.com>
2018-08-27 22:36:36 +0300

    Small cleanup

--- src/fheroes2/system/fh2_win32.cpp
+++ src/fheroes2/system/fh2_win32.cpp
@@ -6,12 +6,13 @@
 
 std::vector<std::string> extractArgsVector(int argc, char** argv)
 {
-	std::vector<std::string> vArgv;
-
 	int nArgs;
 	std::wstring commandLine = GetCommandLineW();
 
 	LPWSTR* szArglist = CommandLineToArgvW(commandLine.c_str(), &nArgs);
+
+	std::vector<std::string> vArgv;
+	vArgv.reserve(nArgs);
 	for (int i = 0; i < nArgs; i++)
 	{
 		vArgv.emplace_back(ws2s(szArglist[i]));
